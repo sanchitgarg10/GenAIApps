40f3845f46ce3dadf4d3225dbb926b59be479e54
sanchitgarg10 <142671884+sanchitgarg10@users.noreply.github.com>
2024-01-16 19:30:07 +0000

    Langanalysis_attemp2

--- app.py
+++ app.py
@@ -16,10 +16,14 @@ ai_client = TextAnalyticsClient(endpoint=endpoint, credential=credential)
 @app.route('/sentiment-analysis', methods=['GET', 'POST'])
 def sentiment_analysis():
     if request.method == 'POST':
-        text_to_analyze = request.form['text_to_analyze']
-        response = ai_client.analyze_sentiment(documents=[text_to_analyze])
-        return render_template('sentiment_analysis.html', sentiment=response[0].sentiment, key_phrases=response[0].key_phrases)
-    return render_template('sentiment_analysis.html', sentiment=None)
+        text_to_analyze = [request.form['text_to_analyze']]
+        sentiment_response = ai_client.analyze_sentiment(documents=text_to_analyze)
+        key_phrases_response = ai_client.extract_key_phrases(documents=text_to_analyze)
+        return render_template('sentiment_analysis.html', 
+                               sentiment=sentiment_response[0].sentiment, 
+                               key_phrases=key_phrases_response[0].key_phrases)
+    return render_template('sentiment_analysis.html', sentiment=None, key_phrases=None)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
