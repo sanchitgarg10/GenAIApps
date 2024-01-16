a64c320bdfa1b4e617cf69f26afe49d77aedca22
sanchitgarg10 <sanchitgarg10@gmail.com>
2024-01-16 18:21:02 +0000

    Initial commit

--- app.py
+++ app.py
@@ -96,12 +96,12 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 
 # Default port:
-if __name__ == '__main__':
-    app.run()
+#if __name__ == '__main__':
+#    app.run()
 
 # Or specify port manually:
-'''
+import os
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-'''
+
