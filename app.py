e128cbf114bed18236815bdd06f7879a177fe4de
sanchitgarg10 <142671884+sanchitgarg10@users.noreply.github.com>
2024-01-23 20:03:27 +0000

    Updated Landing page and added subpages

--- app.py
+++ app.py
@@ -11,6 +11,27 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/prodadvid')
+def prodadvid():
+    return render_template('prodadvid.html')
+
+@app.route('/custgpttut')
+def custgpttut():
+    return render_template('custgpttut.html')
+
+@app.route('/exp')
+def exp():
+    return render_template('exp.html')
+
+@app.route('/phi2')
+def phi2():
+    return render_template('phi2.html')
+
+@app.route('/ccgmailplugin')
+def ccgmailplugin():
+    return render_template('ccgmailplugin.html')
+
+
 ##Register blueprints
 app.register_blueprint(sentiment_analysis_bp)
 app.register_blueprint(speechtotext_bp)
